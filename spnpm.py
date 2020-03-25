02478c51f84e12b13a9cd57c0c4a54b7d95642a4
mimas9107 <mimas9107@gmail.com>
2020-03-25 17:51:16 +0800

    modified:   spnpm.py

--- spnpm.py
+++ spnpm.py
@@ -15,25 +15,28 @@ import time
 ##url = "https://www.nownews.com/cat/column"
 url = "https://npm.cpami.gov.tw/bed_1.aspx" ## 雪霸國家公園,登山入園床位查詢
 headers ={
-        "Host": "npm.cpami.gov.tw",
-        "Origin":"https://npm.cpami.gov.tw",
-        "Referer": "https://npm.cpami.gov.tw/bed_menu.aspx",
+        #"Host": "npm.cpami.gov.tw",
+        #"Origin":"https://npm.cpami.gov.tw",
+        "Referer": "https://npm.cpami.gov.tw/bed_1.aspx",
+        "X-MicrosoftAjax":"Delta = true",
+        "X-Requested-With":"XMLHttpRequest",
+        "Content-Type":"application/x-www-form-urlencoded",
         ## "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0"  ### Firefox
         ## "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36" ### Chrome
         "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0",
          }
 # 模擬 browser 作 get動作
-res = requests.get(url, headers=headers)
+# res = requests.get(url, headers=headers)
 # print(res.text)
-soup = bs(res.text,"lxml")
+# soup = bs(res.text,"lxml")
 # print(soup)
-room_dict={}
+# room_dict={}
 
-for ele in soup.select("div.form-group select option"): ## 幫我抓 div物件中 class為 form-group >> select物件 >> option物件
-    if ele['value'] !="": 
-    #  print(int(ele['value']),ele.text)
-        room_dict[ele.text]=int(ele['value']) ### 將山屋名稱與編號存入字典 room_dict{}
-        room_dict
+#for ele in soup.select("div.form-group select option"): ## 幫我抓 div物件中 class為 form-group >> select物件 >> option物件
+#    if ele['value'] !="": 
+#    #  print(int(ele['value']),ele.text)
+#        room_dict[ele.text]=int(ele['value']) ### 將山屋名稱與編號存入字典 room_dict{}
+#        room_dict
 
 def parsing_hidden_params(soup):
     __VIEWSTATE = soup.select("input#__VIEWSTATE")[0]['value']
@@ -46,21 +49,34 @@ def parsing_hidden_params(soup):
              }
     return output
 
-p = parsing_hidden_params(soup)
-# print(p)
-
-payload={}
-payload["__VIEWSTATE"] = p["__VIEWSTATE"]
-payload["__VIEWSTATEGENERATOR"] = p["__VIEWSTATEGENERATOR"]
-payload["__EVENTVALIDATION"] = p["__EVENTVALIDATION"]
-payload["ctl00$ContentPlaceHolder1$rooms"] = 13
-payload["ctl00$ContentPlaceHolder1$ddlYear"] = 2020
-payload["ctl00$ContentPlaceHolder1$ddlMonth"] = 4
-payload["ctl00$ContentPlaceHolder1$hidYear"] = 2020
-payload["ctl00$ContentPlaceHolder1$hidMonth"] = 3
-payload["ctl00$ContentPlaceHolder1$btnsearch"] = "查詢"
-
-result = requests.post(url,data=payload)
-soup = bs(result.text,"lxml")
-print(soup)
+## -- main --
+with requests.session() as s:
+    res = s.get(url,headers=headers)
+    soup = bs(res.content, "lxml")
+    p = parsing_hidden_params(soup)
+    # print(p)
+    payload={}
+    payload["ctl00$ScriptManager1"]="ctl00$ScriptManager1|ctl00$ContentPlaceHolder1$btnsearch"
+    payload["__EVENTTARGET"]=""
+    payload["__EVENTARGUMENT"]=""
+    payload["__VIEWSTATE"] = p["__VIEWSTATE"]
+    payload["__VIEWSTATEGENERATOR"] = p["__VIEWSTATEGENERATOR"]
+    payload["__EVENTVALIDATION"] = p["__EVENTVALIDATION"]
+    payload["ctl00$txSerch"]=""
+    payload["ctl00$ContentPlaceHolder1$rooms"] = 13
+    payload["ctl00$ContentPlaceHolder1$ddlYear"] = 2020
+    payload["ctl00$ContentPlaceHolder1$ddlMonth"] = 3
+    payload["ctl00$ContentPlaceHolder1$hidYear"] = 2020
+    payload["ctl00$ContentPlaceHolder1$hidMonth"] = 3
+    payload["__ASYNCPOST"]="true"
+    payload["ctl00$ContentPlaceHolder1$btnsearch"] = "查詢"
+    #print(payload)
+    print("="*80)
+    #print(res.cookies.items())
+    #headers["Cookie"]=res.cookies
+    result = requests.post(url,data=payload,headers=headers,allow_redirects=False)
+    #result = requests.post(url,data=payload,timeout=3.0)
+    soup2 = bs(result.text,"lxml")
+    #print(soup.select("div#ContentPlaceHolder1_CalendarDiv"))
+    print(soup2)
 
